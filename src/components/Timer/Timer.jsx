26771bb734b1b3f14a88f2374cc2fd26c9ea8bc0
minung.han <hmu332233@gmail.com>
2019-03-07 03:19:44 +0900

    [Add] Timer component

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Timer.scss';
 
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  render() {
-    return (
-      <div className={styles.Timer}>
+function Timer(props) {
+  return (
+    <div className={styles.Timer}>
+      <div className={styles.Timer__border}>
+        <div className={styles.Timer__inner}>
+          <div className={styles.Round}>
+            <div className={styles.Round__timer} style={{ background: `conic-gradient(white ${props.percent}%, red 0, red 100%)`}} />
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Timer.propTypes = {
+  percent: PropTypes.number,
 };
 Timer.defaultProps = {
+  percent: 0
 };
 
 export default Timer;
